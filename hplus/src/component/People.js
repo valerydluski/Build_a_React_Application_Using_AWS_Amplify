56dc30c37a3f817478bd588825942be74b02c3cd
Valery Dluski <valerydluski@gmail.com>
2021-03-24 18:01:45 +0300

    feat: Add people to the application

--- hplus/src/component/People.js
+++ hplus/src/component/People.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {API, graphqlOperation}  from 'aws-amplify';
 import PersonCard from './PersonCard'
-import peopleData from './data/people.json'
+// import peopleData from './data/people.json'
+
+const listPersons = `
+  query ListPersons{
+    listPersons {
+      items {
+        id
+        fullName
+        dynamicSlug
+        title
+        description
+      }
+    }
+  }
+`
 
 const People = () => {
+  const [peopleData, setPeopleData] = useState([]);
+
+  const loadPersonData = async() => {
+    const { data } = await API.graphql(
+      graphqlOperation(listPersons)
+    );
+    setPeopleData(data?.listPersons?.items);
+  }
+
+useEffect(() => {
+  loadPersonData();
+}, [])
+
   return (
     <section id="people" className="section">
       <header className="imageheader"></header>
